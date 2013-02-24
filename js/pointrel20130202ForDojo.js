e191076a848bd9e4eb986bcb39571f29f3eb3c1a
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2013-02-24 10:16:03 -0500

    Added functions to for variable new and delete to dojo pointrel functions; removed encode as utf8 function references from the pointrel object; fixed bug where extension name was determined incorrectly; updated conceptMapWithDojo.js to use plain encode routines now that they are not in the pointrel object.

--- js/pointrel20130202ForDojo.js
+++ js/pointrel20130202ForDojo.js
@@ -22,22 +22,21 @@ define("pointrel", ["dojo/_base/xhr"], function (xhr) {
         var hash = CryptoJS.SHA256(originalDataString);
         var extensionSeperator = ".";
         if (extension == "") extensionSeperator = "";
-        var uri = "pointrel://sha256_" + hash + "_" + originalDataString.length + extensionSeperator + encodedExtension;
+        var uri = "pointrel://sha256_" + hash + "_" + originalDataString.length + extensionSeperator + extension;
 
         var request = {
             url: "server/resource-add.php",
             // Need to pass original data string as it will be utf-8 encoded by dojo
             content: {"resourceURI": uri, "resourceContent": base64_encode(originalDataString), "userID": $.pointrel_authentication.getUserIDOrAnonymous()},
             headers: { "Content-Type": "application/x-www-form-urlencoded; charset=utf-8" },
-            handleAs: "text",
+            handleAs: "json",
             load: function (data) {
                 //alert("POST result: '" + data + "'");
                 // document.getElementById("response").innerHTML = data; // JSON.stringify(data);
-                var parsedData = JSON.parse(data);
-                if (parsedData.status == "OK") {
-                    if (typeof(callback) == "function") callback(null, parsedData);
+                if (data.status == "OK") {
+                    if (typeof(callback) == "function") callback(null, data);
                 } else {
-                    if (typeof(callback) == "function") callback("FAILED", parsedData);
+                    if (typeof(callback) == "function") callback("FAILED", data);
                 }
             },
             error: function (error, other) {
@@ -80,21 +79,50 @@ define("pointrel", ["dojo/_base/xhr"], function (xhr) {
         // document.getElementById("query").innerHTML = "Waiting... on " + JSON.stringify(request);
     }
 
+    function pointrel_variable_new(variableName, newVersionURI, callback) {
+        console.log("pointrel_resource_new: " + variableName + "new: " + newVersionURI);
+        // var encodedVariableName = encodeAsUTF8(variableName);
+        var request = {
+            url: "server/variable-query.php",
+            content: {"variableName": variableName, "operation": "new", "newValue": newVersionURI, "userID": $.pointrel_authentication.getUserIDOrAnonymous()},
+            handleAs: "json",
+            load: function (data) {
+                // alert("GET result: '" + data + "'");
+                // document.getElementById("retrieve").innerHTML = data;
+                if (data.status == "OK") {
+                    if (typeof(callback) == "function") callback(null, data);
+                } else {
+                    if (typeof(callback) == "function") callback("FAILED", data);
+                }
+            },
+            error: function (error, other) {
+                console.log("error", error, other);
+                alert("POST variable new error: " + error);
+                // TODO: improve error reporting
+                if (typeof(callback) == "function") callback("ERROR", error, other);
+            }
+        };
+
+        xhr.post(request);
+
+        // alert("sent request: " + JSON.stringify(request));
+        // document.getElementById("query").innerHTML = "Waiting... on " + JSON.stringify(request);
+    }
+
     function pointrel_variable_get(variableName, callback) {
         console.log("pointrel_variable_get: " + variableName);
         // var encodedVariableName = encodeAsUTF8(variableName);
         var request = {
             url: "server/variable-query.php",
             content: {"variableName": variableName, "operation": "get", "userID": $.pointrel_authentication.getUserIDOrAnonymous()},
-            handleAs: "text",
+            handleAs: "json",
             load: function (data) {
                 // alert("GET result: '" + data + "'");
                 // document.getElementById("retrieve").innerHTML = data;
-                var parsedData = JSON.parse(data);
-                if (parsedData.status == "OK") {
-                    if (typeof(callback) == "function") callback(null, parsedData);
+                if (data.status == "OK") {
+                    if (typeof(callback) == "function") callback(null, data);
                 } else {
-                    if (typeof(callback) == "function") callback("FAILED", parsedData);
+                    if (typeof(callback) == "function") callback("FAILED", data);
                 }
             },
             error: function (error, other) {
@@ -117,15 +145,14 @@ define("pointrel", ["dojo/_base/xhr"], function (xhr) {
         var request = {
             url: "server/variable-query.php",
             content: {"variableName": variableName, "operation": "set", "currentValue": oldVersionURI, "newValue": newVersionURI, "userID": $.pointrel_authentication.getUserIDOrAnonymous()},
-            handleAs: "text",
+            handleAs: "json",
             load: function (data) {
                 // alert("GET result: '" + data + "'");
                 // document.getElementById("retrieve").innerHTML = data;
-                var parsedData = JSON.parse(data);
-                if (parsedData.status == "OK") {
-                    if (typeof(callback) == "function") callback(null, parsedData);
+                if (data.status == "OK") {
+                    if (typeof(callback) == "function") callback(null, data);
                 } else {
-                    if (typeof(callback) == "function") callback("FAILED", parsedData);
+                    if (typeof(callback) == "function") callback("FAILED", data);
                 }
             },
             error: function (error, other) {
@@ -142,11 +169,41 @@ define("pointrel", ["dojo/_base/xhr"], function (xhr) {
         // document.getElementById("query").innerHTML = "Waiting... on " + JSON.stringify(request);
     }
 
+    function pointrel_variable_delete(variableName, oldVersionURI, callback) {
+        console.log("pointrel_resource_set: " + variableName + " old: " + oldVersionURI);
+        // var encodedVariableName = encodeAsUTF8(variableName);
+        var request = {
+            url: "server/variable-query.php",
+            content: {"variableName": variableName, "operation": "delete", "currentValue": oldVersionURI, "userID": $.pointrel_authentication.getUserIDOrAnonymous()},
+            handleAs: "json",
+            load: function (data) {
+                // alert("GET result: '" + data + "'");
+                // document.getElementById("retrieve").innerHTML = data;
+                if (data.status == "OK") {
+                    if (typeof(callback) == "function") callback(null, data);
+                } else {
+                    if (typeof(callback) == "function") callback("FAILED", data);
+                }
+            },
+            error: function (error, other) {
+                console.log("error", error, other);
+                alert("POST variable delete error: " + error);
+                // TODO: improve error reporting
+                if (typeof(callback) == "function") callback("ERROR", error, other);
+            }
+        };
+
+        xhr.post(request);
+
+        // alert("sent request: " + JSON.stringify(request));
+        // document.getElementById("query").innerHTML = "Waiting... on " + JSON.stringify(request);
+    }
+
     pointrel.resource_add = pointrel_resource_add;
     pointrel.resource_get = pointrel_resource_get;
+    pointrel.variable_new = pointrel_variable_new;
     pointrel.variable_get = pointrel_variable_get;
     pointrel.variable_set = pointrel_variable_set;
-    pointrel.encodeAsUTF8 = encodeAsUTF8;
-    pointrel.decodeFromUTF8 = decodeFromUTF8;
+    pointrel.variable_delete = pointrel_variable_delete;
     return pointrel;
 });
