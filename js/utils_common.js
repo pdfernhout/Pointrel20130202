506e193d4289243ed616832a20df60b2cbf0ce31
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2013-02-24 09:03:03 -0500

    Moved base64 and unicode encoding/decoding code from pointrel lib files to utils_common.js; updated all html file headers to include utils_common.js if needed; also ensured all script tags had text/javascript for the type if needed (for HTML4 it is required although it is the default for HTML5); other cleanup in headers to be more consistent.

--- js/utils_common.js
+++ js/utils_common.js
@@ -110,3 +110,79 @@ Math.uuidFast = function() {
 	}
 	return uuid.join('');
 };
+
+
+/// encoding and decoding
+
+function encodeAsUTF8(text) {
+    return unescape(encodeURIComponent(text));
+}
+
+function decodeFromUTF8(text) {
+    return decodeURIComponent(escape(text));
+}
+
+// From: http://phpjs.org/functions/base64_encode/
+function base64_encode(data) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Tyler Akins (http://rumkin.com)
+    // +   improved by: Bayron Guevara
+    // +   improved by: Thunder.m
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Pellentesque Malesuada
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Rafał Kukawski (http://kukawski.pl)
+    // *     example 1: base64_encode('Kevin van Zonneveld');
+    // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+    // mozilla has this native
+    // - but breaks in 2.0.0.12!
+    //if (typeof this.window['btoa'] == 'function') {
+    //    return btoa(data);
+    //}
+    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    //noinspection JSUnusedAssignment
+    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+        ac = 0,
+        enc = "",
+        tmp_arr = [];
+
+    if (!data) {
+        return data;
+    }
+
+    do { // pack three octets into four hexets
+        o1 = data.charCodeAt(i++);
+        o2 = data.charCodeAt(i++);
+        o3 = data.charCodeAt(i++);
+
+        bits = o1 << 16 | o2 << 8 | o3;
+
+        h1 = bits >> 18 & 0x3f;
+        h2 = bits >> 12 & 0x3f;
+        h3 = bits >> 6 & 0x3f;
+        h4 = bits & 0x3f;
+
+        // use hexets to index into b64, and append result to encoded string
+        tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+    } while (i < data.length);
+
+    enc = tmp_arr.join('');
+
+    var r = data.length % 3;
+
+    return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+}
+
+function validateBinaryData(dataString) {
+    // slow for now...
+    for (var i = 0; i < dataString.length; i++) {
+        var c = dataString.charAt(i);
+        // console.log("char", i, c.charCodeAt(0), c);
+        var charCode = c & 0xff;
+        if (charCode < 0 || charCode > 255) {
+            alert("string had data outside the range of 0-255 at position: " + i);
+            return false;
+        }
+    }
+    return true;
+}
