664002ec38a98edf79466fd0241594bedc953138
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-03-04 06:50:41 -0500

    Added more error reporting to journal tests; getting all journal tests to work without errors.

--- test/journal/test_create.php
+++ test/journal/test_create.php
@@ -1,8 +1,14 @@
 <?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 chdir("../../pointrel/pointrel-app/server/");
+
+$_SERVER['REQUEST_METHOD'] = "POST";
+$_SERVER['REMOTE_ADDR'] = "localhost-TEST";
 
 $_POST['userID'] = "pdfernhout@example.com";
 $_POST['journalName'] = "test_foo";
 $_POST['operation'] = "create";
+$_POST['journalFormat'] = "journal";
 
 include "journal-store.php";
--- test/journal/test_exists_fail.php
+++ test/journal/test_exists_fail.php
@@ -1,5 +1,10 @@
 <?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 chdir("../../pointrel/pointrel-app/server/");
+
+$_SERVER['REQUEST_METHOD'] = "POST";
+$_SERVER['REMOTE_ADDR'] = "localhost-TEST";
 
 $_POST['userID'] = "pdfernhout@example.com";
 $_POST['journalName'] = "test_foo_fail";
