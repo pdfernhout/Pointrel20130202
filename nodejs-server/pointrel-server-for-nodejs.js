7234c1430f6af98f1851e0517dd7953de674efd5
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-11-04 08:58:02 -0500

    Improving nodejs server stub

--- nodejs-server/pointrel-server-for-nodejs.js
+++ nodejs-server/pointrel-server-for-nodejs.js
@@ -1,4 +1,5 @@
 // Test at: http://localhost:8080/pointrel/pointrel-app/
+"use strict";
 
 console.log("Pointrel20130202 server for nodejs started: " + Date());
 
@@ -7,6 +8,16 @@ console.log("__dirname", __dirname);
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/..'));
+app.use("/pointrel", express.static(__dirname + '/pointrel'));
 
-app.listen(8080);
+app.get('/', function (request, response) {
+  response.send('Hello World!');
+});
+	
+var server = app.listen(8080,function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Pointrel20130202 app listening at http://%s:%s', host, port);
+});
